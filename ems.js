3cfea593e09608d6c64040c498191196707832bd
Janani NA <janani2285@gmail.com>
2020-07-14 22:44:09 -0700

    Adding new role

--- ems.js
+++ ems.js
@@ -2,6 +2,8 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table");
 
+const selectQueryDept = "SELECT * FROM department";
+const insertQueryDept = "INSERT INTO department SET dept_name=?";
 
 const connection = mysql.createConnection({
     host: "localhost",
@@ -65,26 +67,74 @@ async function addNewDept() {
             name: "newDeptName"
         },
     ]);
-   
-    const query = "INSERT INTO department SET dept_name=?";
-    connection.query(query, answers.newDeptName, function (err, res) {
-      if (err) {
-        console.log("ERROR occurred during inserting new department into database. " + err);
-        }else{
+
+
+    connection.query(insertQueryDept, answers.newDeptName, function (err, res) {
+        if (err) {
+            console.log("ERROR occurred during inserting new department into database. " + err);
+        } else {
             console.log(`SUCCESS!!!! ${answers.newDeptName} has been added to department table`);
         }
         mainMenu();
     });
 }
 
-function  viewDept(){
-    const query = "SELECT * FROM department";
-    connection.query(query, function (err, res) {
-      if (err) {
-        console.log("ERROR occurred while retriving department data from database. " + err);
-        }else{
+
+
+function viewDept() {
+
+    connection.query(selectQueryDept, function (err, res) {
+        if (err) {
+            console.log("ERROR occurred while retriving department data from database. " + err);
+        } else {
             console.table(res);
         }
         mainMenu();
     });
 }
+
+function addNewRole() {
+    connection.query(selectQueryDept, function (err, res) {
+        if (err) {
+            console.log("Adding new role - ERROR occurred while retriving department data from database. " + err);
+        } else {
+            const deptArray = res.map((row) => row.dept_name);
+            const answers = inquirer.prompt([
+                {
+                    type: "input",
+                    message: "Enter the new role you like to add:",
+                    name: "newRole"
+                },
+                {
+                    type: "input",
+                    message: "Enter salary for the new role:",
+                    name: "newSalary"
+                },
+                {
+                    type: "list",
+                    message: "Choose the department",
+                    choices: deptArray,
+                    name: "dept"
+                }
+            ]).then(answers => {
+               
+                 const query = "INSERT INTO role SET title=?, salary=?, dept_id=(SELECT dept_id from department WHERE dept_name = ?)";
+
+                connection.query(query, [answers.newRole, answers.newSalary, answers.dept], function (err, res) {
+                    if (err) {
+                        console.log("ERROR occurred during inserting new role into database. " + err);
+                    } else {
+                        console.log(`SUCCESS!!!! ${answers.newRole} role in ${answers.dept} department has been added to Role table with ${answers.newSalary} salary`);
+                    }
+                    mainMenu();
+                }); 
+            })
+
+
+        }
+        //   mainMenu();
+    });
+
+
+}
+
