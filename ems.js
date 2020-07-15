ab603e4a63173c0ac0a775e9e63b4b14d00c6ca0
Janani NA <janani2285@gmail.com>
2020-07-14 19:14:51 -0700

    New dept added functionality added

---
+++ ems.js
@@ -0,0 +1,78 @@
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+require("console.table");
+
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "emsDB",
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.log("ERROR connecting to database. " + err);
+    }
+    console.log("connected as id " + connection.threadId);
+    mainMenu();
+});
+
+async function mainMenu() {
+    // console.log("Inside Main menu");
+    const answers = await inquirer.prompt([
+        {
+            type: "list",
+            message: "What would you like to do?",
+            choices: ["Add New Department", "View Departments", "Add New Role", "View Roles", "Add New Employee", "View Employee", "Update Employee Role", "Exit"],
+            name: "userChoice"
+        }
+    ]);
+    switch (answers.userChoice) {
+        case "Add New Department":
+            addNewDept();
+            break;
+        case "View Departments":
+            viewDept();
+            break;
+        case "Add New Role":
+            addNewRole();
+            break;
+        case "View Roles":
+            viewRole();
+            break;
+        case "Add New Employee":
+            addNewEmp();
+            break;
+        case "View Employee":
+            viewEmp();
+            break;
+        case "Update Employee Role":
+            updateEmpRole();
+            break;
+        case "Exit":
+            connection.end();
+            break;
+    }
+}
+
+async function addNewDept() {
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter the new department you like to add:",
+            name: "newDeptName"
+        },
+    ]);
+    console.log("answers.newDeptName--",answers.newDeptName)
+    const query = "INSERT INTO department SET dept_name=?";
+    connection.query(query, answers.newDeptName, function (err, res) {
+      if (err) {
+        console.log("ERROR occurred during inserting new department into database. " + err);
+        }else{
+            console.log(`SUCCESS!!!! ${answers.newDeptName} has been added to department table`);
+        }
+        mainMenu();
+    });
+}
--- emsSeeds.sql
+++ emsSeeds.sql
@@ -1,29 +1,65 @@
 USE emsDB;
+-- Department
+INSERT INTO department (dept_name) VALUES ("Sales");
 
-INSERT INTO department (dept_name)
-VALUES ("Sales");
+INSERT INTO department (dept_name) VALUES ("Human Resource");
 
-INSERT INTO department (dept_name)
-VALUES ("Human Resource");
+INSERT INTO department (dept_name) VALUES ("Production");
 
-INSERT INTO department (dept_name)
-VALUES ("Production");
+INSERT INTO department (dept_name) VALUES ("Engineering");
 
+-- INSERT INTO department (dept_name) VALUES ("Finance");
+
+-- INSERT INTO department (dept_name) VALUES ("Legal");
+
+
+-- Role
+INSERT INTO role (title, salary, dept_id)
+VALUES ("Sales Manager", 100000,1);
 
 INSERT INTO role (title, salary, dept_id)
-VALUES ("Manager", 1000.00,1);
+VALUES ("Sales Person", 50000,1);
 
 INSERT INTO role (title, salary, dept_id)
-VALUES ("Intern", 200.00,1);
+VALUES ("HR Lead", 250000,2);
 
 INSERT INTO role (title, salary, dept_id)
-VALUES ("Manager", 250.00,2);
+VALUES ("HR Intern", 25000,2);
 
 INSERT INTO role (title, salary, dept_id)
-VALUES ("Team Lead", 250.00,3);
+VALUES ("Account Manager", 150000,4);
 
-INSERT INTO employee (first_name, last_name, role_id)
-VALUES ("Tom","Samuel", 1);
+INSERT INTO role (title, salary, dept_id)
+VALUES ("Accountant", 100000,4);
+
+INSERT INTO role (title, salary, dept_id)
+VALUES ("Team Lead", 150000,4);
+
+INSERT INTO role (title, salary, dept_id)
+VALUES ("Software Engineer ", 100000,4);
+
+
+-- Employee
+INSERT INTO employee (first_name, last_name, role_id,manager_id)
+VALUES ("Tom","Samuel", 1,null);
 
 INSERT INTO employee (first_name, last_name, role_id,manager_id)
-VALUES ("Deena","Arun", 2,1);
+VALUES ("Deena","Sue", 2,1);
+
+INSERT INTO employee (first_name, last_name, role_id,manager_id)
+VALUES ("Rebbaca","Travor", 3,null);
+
+INSERT INTO employee (first_name, last_name, role_id,manager_id)
+VALUES ("Rhonda","Higgns", 4,3);
+
+INSERT INTO employee (first_name, last_name, role_id,manager_id)
+VALUES ("Glenda","Samuel", 5,null);
+
+INSERT INTO employee (first_name, last_name, role_id,manager_id)
+VALUES ("Raj","Kumar", 6,5);
+
+INSERT INTO employee (first_name, last_name, role_id,manager_id)
+VALUES ("Vyas","Rajkumar", 7,null);
+
+INSERT INTO employee (first_name, last_name, role_id,manager_id)
+VALUES ("Tim","Giby", 8,7);
